0aefe983e4d8e37c40f7ed4c541d1b4d5a5c19dc
luis <contatoluisfernandogaleano@gmail.com>
2025-04-28 09:52:01 -0400

    Adicionando testes unitarios para registros de servicos

---
+++ CommonTestUtilities/Requests/RequestRegisterFaturamentoJsonBuilder.cs
@@ -0,0 +1,16 @@
+﻿using Barber.Communication.Enum;
+using Barber.Communication.Request;
+using Bogus;
+
+namespace CommonTestUtilities.Requests;
+public class RequestRegisterFaturamentoJsonBuilder
+{
+    public static RequestFaturamentoJson Build()
+    {
+        return new Faker<RequestFaturamentoJson>()
+            .RuleFor(r => r.Servicos, f => f.PickRandom<Servicos>())
+            .RuleFor(r => r.Data, f => f.Date.Past())
+            .RuleFor(r => r.FormaPagamento, f => f.PickRandom<FormaPagamento>())
+            .RuleFor(r => r.Valor, f => f.Random.Decimal(1, 10000));
+    }
+}
---
+++ Validator.Tests/Faturamento/Register/RegisterFaturamentoValidatorTests.cs
@@ -0,0 +1,74 @@
+﻿using Barber.Application.UseCases.Faturamento;
+using Barber.Communication.Enum;
+using CommonTestUtilities.Requests;
+using FluentAssertions;
+
+namespace Validator.Tests.Faturamento.Register;
+public class RegisterFaturamentoValidatorTests
+{
+    [Fact]
+    public void Success()
+    {
+        //Arrange
+        var validator = new FaturamentoValidator();
+        var request = RequestRegisterFaturamentoJsonBuilder.Build();
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeTrue();   
+    }
+
+
+
+    [Fact]
+    public void Error_Service_Type()
+    {
+        //Arrange
+        var validator = new FaturamentoValidator();
+        var request = RequestRegisterFaturamentoJsonBuilder.Build();
+        request.Servicos = (Servicos)10;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle();
+    }
+
+    [Fact]
+    public void Error_Payment_Type()
+    {
+        //Arrange
+        var validator = new FaturamentoValidator(); 
+        var request = RequestRegisterFaturamentoJsonBuilder.Build();
+        request.FormaPagamento = (FormaPagamento)100;
+
+        //Act
+        var result = validator.Validate(request);
+
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle();
+    }
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-50)]
+    [InlineData(-1)]
+    [InlineData(-10000)]
+    public void Error_Value(decimal valor)
+    {
+        //Arrange
+        var validator = new FaturamentoValidator();
+        var request = RequestRegisterFaturamentoJsonBuilder.Build();
+        request.Valor = valor;
+        //Act
+        var result = validator.Validate(request);
+        //Assert
+        result.IsValid.Should().BeFalse();
+        result.Errors.Should().ContainSingle();
+    }
+
+}
